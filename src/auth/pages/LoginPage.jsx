bbc3bad33a06a26cee05d70dba101bb555dc40ae
Miguel Limón <mili95_gontex@hotmail.com>
2023-10-15 01:27:02 -0600

    Controls added to Login page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,27 +1,59 @@
 import { useNavigate } from 'react-router-dom';
 
+import { useContext, useState } from "react";
+
 export const LoginPage = () => {
 
-  const navigate = useNavigate();
-
-  const onLogin = () => {
-    navigate('/', {
-      replace: true
-    });
-  }
+    //const { login } = useContext( AuthContext );
+    const navigate = useNavigate();
+  
+    
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const onLogin = async (e) => {
+      e.preventDefault();
+      /*
+      if (username != "" &&
+        password != "") {
+  
+        const lastPath = localStorage.getItem('lastPath') || '/';
+  
+        const response = await login(username, password); 
+        console.log(response)
+        navigate(lastPath, {
+          replace: true
+        });
+      }*/
+    }
 
   return (
-    <div className="container mt-5">
-      <h1>Login page</h1>
-      <hr />
-
-      <button 
-        className="btn btn-primary"
-        onClick={ onLogin }
-      >
-        Login
-      </button>
-
-    </div>
+      <div className="p-2">
+          <h1>Restaurant App</h1>
+          <hr />
+          <h5>Login</h5>
+          <form onSubmit={onLogin}>
+              <input type="text" id="inputUser" className="form-control"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => {
+                      setUsername(e.target.value);
+                  }}
+                  required autoFocus />
+              <input type="password" id="inputPassword" className="form-control"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => {
+                      setPassword(e.target.value);
+                  }}
+                  required />
+              <br />
+              <button className="btn btn-primary"
+                  type="submit"
+                  onClick={onLogin}>
+                  Entrar
+              </button>
+          </form>
+      </div>
   )
 }
